7abf2a960d468623eb344ec8dff0e547e7d6b81a
Marin <marin.postma@gmail.com>
2017-05-28 19:40:09 +0300

    test

---
+++ SleepVestArduino/SleepVestArduino.ino
@@ -0,0 +1,56 @@
+//test
+/* [IMPORTANT] PLEASE LOG THE UPDATES HERE :
+ * v0.0 describing the function, skeleton for the script
+ * 
+ */
+
+
+//TODO: implement connection (might be bluetooth) or connection to nodemcu, (might be serial connection)...to be determined
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+// TODO: Function to make noise to wake up (optional) :
+
+void ring(){
+  
+}
+
+//TODO : function to light leds in hood to wake up
+
+void lightHood() {
+  
+}
+
+//TODO: deflate the hood (open the valve for manual deflation, the valse should remain opened during all wake up time)
+
+void deflateHood(){
+  
+}
+
+//TODO : inflate the hood (we will have to experimentaly count the tie needed to inflate the hood and then set it in the function) :
+void inflateHood() {
+  
+}
+
+//TODO: function for the activation of the led stipes, this takes in argument an Integer : 0 when you just woke up, max when you are about to sleep
+void ledStripe(int level){
+  
+}
+
+//TODO : this funtion sends a notification to check if person is up or not
+
+bool isUp(){
+
+}
+int i = 0
+
+
