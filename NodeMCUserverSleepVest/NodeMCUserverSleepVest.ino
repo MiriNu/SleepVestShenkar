d7755956148810a3d2a749a4e626a50c5ec1c2c8
Marin <marin.postma@gmail.com>
2017-05-28 21:22:55 +0300

    added nodeMCU script

---
+++ NodeMCUserverSleepVest/NodeMCUserverSleepVest.ino
@@ -0,0 +1,90 @@
+/*
+ * 
+ * this is a nodemcu script to interact with iphone app
+ * 
+ * 
+
+*/
+
+
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "Home";
+const char* password = "af12345678";
+
+String state = "-1";
+int ledPin = 13; // GPIO13
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+
+
+ if (request != state && request.length() == 3){
+  state = request;
+  Serial.print(request);
+ }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+ 
+  
+ 
+}
+ 
